01f3d8cf013406f4e3d80e8f77b5bc65a60e2cd2
Kawan Mark Silva <128873271+KawanMark@users.noreply.github.com>
2023-06-12 22:52:17 -0300

    Add files via upload

---
+++ Click a color/index.js
@@ -0,0 +1,18 @@
+const colors = ['Red', 'black', 'Blue', 'Yellow', 'Purple', 'Green']
+const btn = document.getElementById('btn')
+const color = document.querySelector('.color')
+
+btn.addEventListener('click', function(){
+    const randomColor = getRandomColor()
+    document.body.style.background = colors[randomColor]
+    color.textContent = colors[randomColor]
+})
+
+function getRandomColor(){
+    return Math.floor(Math.random() * colors.length)
+}
+
+
+function youColor(){
+    document.getElementById('color').innerHTML = 'Azul'
+}
